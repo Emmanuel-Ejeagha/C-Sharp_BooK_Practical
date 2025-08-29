94112e1dca264f2824bf380ab5206704237f9bb8
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2025-08-29 12:18:00 +0100

    Invoked PassingArgOfRefType Methos

---
+++ Chpt9_Methods/Program.cs
@@ -0,0 +1,18 @@
+﻿using Chpt9_Methods;
+
+// Invoking PrintTotalAmount Method
+decimal[] prices = { 19.95m, 25.50m, 8.99m, 12.75m, 5.49m };
+
+PrintTotalAmountForBooks.PrintTotalAmount(prices);
+
+
+int[] arrArg = { 1, 2, 3 };
+
+Console.Write("Before ModifyArray() the argument is: ");
+PassingArgOfRefType.PrintArray(arrArg);
+
+// Modifying the array's argument
+PassingArgOfRefType.ModifyArray(arrArg);
+
+Console.Write("After ModifyArray() the argument is: ");
+PassingArgOfRefType.PrintArray(arrArg);
