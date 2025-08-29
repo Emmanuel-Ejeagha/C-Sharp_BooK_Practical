3c4de93001c694cd50dbbe121db21ff7e80cce7e
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2025-08-29 12:20:36 +0100

    Print Total Amt of books method

---
+++ Chpt9_Methods/PrintTotalAmountForBooks.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Chpt9_Methods;
+
+public class PrintTotalAmountForBooks
+{
+    // This method calculates and prints the total amount for an array of book prices
+    public static void PrintTotalAmount(decimal[] prices)
+    {
+        decimal totalAmount = 0;
+
+        foreach (decimal singleBookPrice in prices)
+        {
+            totalAmount += singleBookPrice;
+        }
+        Console.WriteLine("The total amount for all books is: $" + totalAmount);
+    }
+}
