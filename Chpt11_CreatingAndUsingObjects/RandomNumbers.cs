c4f46bd41a9ee03d12a5ab04235c042586d798d4
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2025-09-02 13:14:00 +0100

    Add: Random numbers

---
+++ Chpt11_CreatingAndUsingObjects/RandomNumbers.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Chpt11_CreatingAndUsingObjects;
+
+public class RandomNumbers
+{
+    public static void PrintRandomNumbers()
+    {
+
+        Random rand = new Random();
+        for (int number = 1; number <= 6; number++)
+        {
+            int randomNumber = rand.Next(19) + 1;
+            Console.Write($"{randomNumber} ");
+        }
+        Console.WriteLine();
+    }
+}
