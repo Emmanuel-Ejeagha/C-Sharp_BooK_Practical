22291130ee7b4139aa95000fa0464d08372b18c5
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2025-09-02 11:42:11 +0100

    Added: Time test

---
+++ Chpt11_CreatingAndUsingObjects/Program.cs
@@ -0,0 +1,23 @@
+﻿using Chpt11_CreatingAndUsingObjects;
+
+// Test the time it takes to execute this code
+int startTime = Environment.TickCount;
+
+Cat.Run();
+
+// System Test
+int endTime = Environment.TickCount;
+Console.WriteLine("The time elapsed is {0} sec.", (endTime - startTime) / 1000.0);
+
+// Test the time it takes to execute this loop
+startTime = Environment.TickCount;
+
+int sum = 0;
+for (int i = 0; i <= 10000000; i++)
+{
+    sum++;
+}
+
+endTime = Environment.TickCount;
+
+Console.WriteLine("The time elapsed is {0} sec.", (endTime - startTime) / 1000.0);
