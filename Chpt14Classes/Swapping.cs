2616fa89aeea8a6335b90bcf1cef4fbd798010ed
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2025-09-08 18:10:27 +0100

    Added Swappint

---
+++ Chpt14Classes/Swapping.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace Chpt14Classes;
+
+public class Swapping
+{
+    public void Swap<K>(ref K a, ref K b)
+    {
+        K oldA = a;
+        a = b;
+        b = oldA;
+    }
+    public void PrintSwap()
+    {
+
+        int num1 = 3;
+        int num2 = 5;
+
+        Console.WriteLine("Before swap: {0} {1}", num1, num2);
+        Swap(ref num1, ref num2);
+        Console.WriteLine("After swap: {0} {1}", num1, num2);
+
+        string str1 = "Hello";
+        string str2 = "There";
+
+        Console.WriteLine("Before swap: {0} {1}", str1, str2);
+        Swap(ref str1, ref str2);
+        Console.WriteLine("After swap: {0} {1}", str1, str2);
+
+    }
+}
