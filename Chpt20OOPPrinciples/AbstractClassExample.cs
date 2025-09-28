1ffda1e044eab7ff4a44dcc5d2be3364a60f424a
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2025-09-28 11:35:05 +0100

    Added: Abstract Class method

---
+++ Chpt20OOPPrinciples/AbstractClassExample.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Chpt20OOPPrinciples;
+
+public class AbstractClassExample
+{
+    public static void PrintAbstractClassExample()
+    {
+        Animal cat = new Cat();
+        cat.PrintInformation();
+    }
+}
