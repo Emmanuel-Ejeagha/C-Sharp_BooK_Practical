fced9a9eb864bba1d94e349df6a7fc2f2c0dbcaa
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2025-09-28 11:43:01 +0100

    Added: Main file

---
+++ Chpt20OOPPrinciples/Program.cs
@@ -0,0 +1,20 @@
+﻿using Chpt20OOPPrinciples;
+
+Console.WriteLine(new object());
+// Console.WriteLine(new Felidae(true));
+Console.WriteLine(new Lion(true, 80));
+
+OverrideExample.HowToOverride();
+
+// Call the cat class. AbstractClas Example
+AbstractClassExample.PrintAbstractClassExample();
+
+// Virtual Methods Example
+Lion lion = new Lion(true, 80);
+lion.CatchPrey(null);
+
+AfricanLion lion1 = new AfricanLion(true, 120);
+lion1.CatchPrey(null);
+
+Lion lion2 = new AfricanLion(false, 50);
+lion2.CatchPrey(null);
