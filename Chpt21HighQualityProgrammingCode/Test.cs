633f55a0dfb29e1924a552fef614e7ec62243c0d
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2025-09-30 12:02:22 +0100

    Test file

---
+++ Chpt21HighQualityProgrammingCode/Test.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace Chpt21HighQualityProgrammingCode;
+
+public class Test
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
