be6a4e74e1b7d9a5bf58408d6d03bfdbad8b72a1
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2025-09-30 12:10:28 +0100

    Added: Main() method

---
+++ Chpt22LamdaExpressionAndLINQ/Program.cs
@@ -0,0 +1,26 @@
+﻿using Chpt22LamdaExpressionAndLINQ;
+
+EvenNumbers.FrindPrimeNumbers();
+
+Dog.GetDogDetails();
+Dog.SortedDogs();
+
+List<int> list = new List<int>() { 20, 1, 4, 8, 9, 44 };
+//  Process each argument with code statements
+var EvenNum = list.FindAll((i) =>
+{
+    Console.WriteLine($"Value of i is: {i}");
+    return (i % 2) == 0;
+});
+
+Func<bool> boolFunc = () => true;
+Func<int, bool> intFunc = (x) => x < 10;
+if (boolFunc() && intFunc(5))
+{
+    Console.WriteLine("5 < 10");
+}
+
+LINQQueryPlayground.PrintNumbers();
+LINQQueryPlayground.PrintSortedElements();
+LINQQueryPlayground.NumberGrouping();
+JoinData.PrintJoinedData();
