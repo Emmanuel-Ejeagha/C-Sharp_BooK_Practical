7c0f96a5b9a5e85e5223be2585a29be9fbe70ef0
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2025-09-30 12:01:35 +0100

    Main file

---
+++ Chpt21HighQualityProgrammingCode/Program.cs
@@ -0,0 +1,18 @@
+﻿using Chpt21HighQualityProgrammingCode;
+
+if (UnitTest.Sum(new int[] { 1, 2 }) != 3)
+    throw new Exception("1 + 2 != 3");
+
+if (UnitTest.Sum(new int[] { 1 }) != 1)
+    throw new Exception("Sum of 1 != 1");
+
+if (UnitTest.Sum(new int[] { }) != 0)
+    throw new Exception("Sum of 0 numbers != 0");
+
+if (UnitTest.Sum(new int[] { -1, -1 }) != -2)
+    throw new Exception("-1 + -1 != -2");
+
+if (UnitTest.Sum(new int[] { 2000000000, 2000000000 }) != 4000000000)
+    throw new Exception("200000000 + 200000000 != 4000000000");
+
+// Console.Write("Hello World");
