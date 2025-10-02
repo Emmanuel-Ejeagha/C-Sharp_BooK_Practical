9f7c83a420013eee21faf1e57e1609231b6ee4ff
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2025-10-02 06:26:56 +0100

    Completed Chpt23

--- Chpt23MethodologyOfProblemSolving/CardShuffle.cs
+++ Chpt23MethodologyOfProblemSolving/CardShuffle.cs
@@ -4,6 +4,8 @@ namespace Chpt23MethodologyOfProblemSolving;
 
 public class CardShuffles
 {
+    static string[] allFaces = new string[] { "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K", "A" };
+    static Suit[] allSuits = new Suit[] { Suit.CLUB, Suit.DIAMOND, Suit.HEART, Suit.SPADE };
     static Random rand = new Random();
     static List<Card> cards = new List<Card>();
     public static void CreateCardShuffle()
@@ -24,12 +26,17 @@ public class CardShuffles
 
         Console.Write("Test Shuffle 52 Cards: ");
         TestShuffle52Cards();
+
+        Console.Write("Test Shuffle 2 Cards: ");
+        TestShuffleTwoCards();
+
+        Console.Write("Test Shuffle 52000 Cards: ");
+        TestShuffle52000Cards();
+
     }
 
     static void TestShuffle52Cards()
     {
-        string[] allFaces = new string[] { "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K", "A" };
-        Suit[] allSuits = new Suit[] { Suit.CLUB, Suit.DIAMOND, Suit.HEART, Suit.SPADE };
         foreach (string face in allFaces)
         {
             foreach (Suit suit in allSuits)
@@ -44,7 +51,7 @@ public class CardShuffles
 
     static void PerformSingleSwap(List<Card> cards, int index)
     {
-        int randomIndex = rand.Next(1, cards.Count);
+        int randomIndex = rand.Next(0, cards.Count);
         Card firstCard = cards[index];
         Card randomCard = cards[randomIndex];
         cards[index] = randomCard;
@@ -53,10 +60,13 @@ public class CardShuffles
 
     static void ShuffleCards(List<Card> cards)
     {
-        for (int i = 0; i < cards.Count; i++)
+        if (cards.Count > 1)
         {
-            PerformSingleSwap(cards, i);
-            // PrintCards(cards);
+            for (int i = 0; i < cards.Count; i++)
+            {
+                PerformSingleSwap(cards, i);
+                // PrintCards(cards);
+            }
         }
     }
 
@@ -67,5 +77,28 @@ public class CardShuffles
             Console.Write(card);
         }
         Console.WriteLine();
+    }
+    static void TestShuffleTwoCards()
+    {
+        cards.Add(new Card() { Face = "A", Suit = Suit.CLUB });
+        cards.Add(new Card() { Face = "3", Suit = Suit.DIAMOND });
+        ShuffleCards(cards);
+        PrintCards(cards);
+    }
+
+    static void TestShuffle52000Cards()
+    {
+
+        for (int i = 0; i < 1000; i++)
+        {
+            foreach (string face in allFaces)
+            {
+                foreach (Suit suit in allSuits)
+                {
+                    Card card = new Card() { Face = face, Suit = suit };
+                    cards.Add(card);
+                }
+            }
+        }
     }
 }
--- Chpt23MethodologyOfProblemSolving/Program.cs
+++ Chpt23MethodologyOfProblemSolving/Program.cs
@@ -4,3 +4,4 @@ Card card = new Card() { Face = "A", Suit = Suit.CLUB };
 Console.WriteLine(card);
 
 CardShuffles.CreateCardShuffle();
+Sort1000Numbers.Sort1000NumbersRandomly();
