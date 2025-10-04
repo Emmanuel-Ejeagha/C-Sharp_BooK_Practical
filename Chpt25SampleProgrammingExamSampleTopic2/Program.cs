a1d3ef019d5425b5fe7fc101ff76be41347df18b
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2025-10-04 11:44:18 +0100

    Main() method

---
+++ Chpt25SampleProgrammingExamSampleTopic2/Program.cs
@@ -0,0 +1,20 @@
+﻿using Chpt25SampleProgrammingExamSampleTopic2.ArithmeticExpression;
+using Chpt25SampleProgrammingExamSampleTopic2.CountingUpperAndLowerCaseWords;
+
+// CountUpperAndLowerCaseWords.TestExtractSeparators();
+// CountUpperAndLowerCaseWords.TestExtractWords();
+// CountUpperAndLowerCaseWords.TestCountWords();
+
+// WordCounter.RunWordCounter();
+
+// // Test IsPrime
+// MatrixOfPrimes.TestIsPrime();
+// MatrixOfPrimes.TestFindNextPrime();
+// MatrixOfPrimes.RunMatrixOFPrimes();
+
+// // //  Test Arithemetic Expression
+// ArithmeticExpression.TestExtractNumbers();
+// ArithmeticExpression.TestExtractOperators();
+// ArithmeticExpression.TestCalculateExpression();
+// ArithmeticExpression.RunArithmeticExpression();
+SimpleExpressionEvaluator.RunSimpleExpressionEvaluator();
