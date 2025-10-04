f17c63adec0640c6eaebae4d96bc8a8e765903a9
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2025-10-04 20:32:29 +0100

    Added: Main() method tests

---
+++ Chpt26SampleProgrammintExamTopic3/Program.cs
@@ -0,0 +1,26 @@
+﻿using Chpt26SampleProgrammintExamTopic3.School;
+
+// Test School
+Student studentPeter = new Student("Peter", "Ikechukwu");
+Console.WriteLine(studentPeter);
+
+// Test Group
+Student studentUche = new Student("Uche", "Uchechukwu");
+Student studentEmeka = new Student("Emeka", "Udeh");
+Group groupEnglish = new Group("English Language Course");
+groupEnglish.Students.Add(studentUche);
+groupEnglish.Students.Add(studentEmeka);
+Teacher teacherEmma = new Teacher("Emmanuel", "Ejeagha");
+groupEnglish.Teacher = teacherEmma;
+Console.WriteLine(groupEnglish);
+
+// Test Teacher
+Teacher teacherMike = new Teacher("Ebele", "Michael");
+Group groupGerman = new Group("German language");
+Group groupFrench = new Group("French language");
+teacherMike.Groups.Add(groupGerman);
+teacherMike.Groups.Add(groupFrench);
+Console.WriteLine(teacherMike);
+
+// Test School
+TestSchool.RunTestSchool();
