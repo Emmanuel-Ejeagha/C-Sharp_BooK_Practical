e45b5f1fb42ee540591ae75e62b107ba076d4b55
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2025-10-04 20:43:07 +0100

    Completed the book

--- Chpt25SampleProgrammingExamSampleTopic2/Program.cs
+++ Chpt25SampleProgrammingExamSampleTopic2/Program.cs
@@ -1,20 +1,21 @@
 ﻿using Chpt25SampleProgrammingExamSampleTopic2.ArithmeticExpression;
 using Chpt25SampleProgrammingExamSampleTopic2.CountingUpperAndLowerCaseWords;
 
-// CountUpperAndLowerCaseWords.TestExtractSeparators();
-// CountUpperAndLowerCaseWords.TestExtractWords();
-// CountUpperAndLowerCaseWords.TestCountWords();
+CountUpperAndLowerCaseWords.TestExtractSeparators();
+CountUpperAndLowerCaseWords.TestExtractWords();
+CountUpperAndLowerCaseWords.TestCountWords();
 
-// WordCounter.RunWordCounter();
+WordCounter.RunWordCounter();
 
-// // Test IsPrime
-// MatrixOfPrimes.TestIsPrime();
-// MatrixOfPrimes.TestFindNextPrime();
-// MatrixOfPrimes.RunMatrixOFPrimes();
+// Test IsPrime
+MatrixOfPrimes.TestIsPrime();
+MatrixOfPrimes.TestFindNextPrime();
+MatrixOfPrimes.RunMatrixOFPrimes();
 
-// // //  Test Arithemetic Expression
-// ArithmeticExpression.TestExtractNumbers();
-// ArithmeticExpression.TestExtractOperators();
-// ArithmeticExpression.TestCalculateExpression();
-// ArithmeticExpression.RunArithmeticExpression();
+// //  Test Arithemetic Expression
+ArithmeticExpression.TestExtractNumbers();
+ArithmeticExpression.TestExtractOperators();
+ArithmeticExpression.TestCalculateExpression();
+ArithmeticExpression.RunArithmeticExpression();
 SimpleExpressionEvaluator.RunSimpleExpressionEvaluator();
+
